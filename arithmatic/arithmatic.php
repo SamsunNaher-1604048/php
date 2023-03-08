8c556dfc18fe87eed5aa8cb96e3e9f60b40d9607
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-03-08 07:53:19 +0600

    constant,arithematic operator,assignment

---
+++ arithmatic/arithmatic.php
@@ -0,0 +1,30 @@
+<?php
+$a=10;
+$b=20;
+
+$c= $a+$b;
+echo $c.'<br>';
+
+$c=$a-$b;
+echo $c.'<br>';
+
+$c = $a*$b;
+echo $c .'<br>';
+
+$c = $a/$b;
+echo $c .'<br>';
+
+$c = $a%$b;
+echo $c.'<br>';
+
+$c= $a**$b;
+echo $c.'<br>';
+
+ $a++;
+echo $a,'<br>';
+
+$a=10.89;
+$b=4;
+$c=$a+$b;
+echo $c;
+?>
---
+++ contant/constant.php
@@ -0,0 +1,8 @@
+<?php
+define('num',50);
+define('_num',10);
+// define('WRONG',100,true);
+// echo WRONG;
+ echo num;
+ echo _num;
+?>
