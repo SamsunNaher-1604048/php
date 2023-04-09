9c3120691967b6eae5bb410854dcba20965ba63e
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-04-09 20:12:55 +0600

    string

---
+++ string/Str_split and chunk_split.php
@@ -0,0 +1,17 @@
+<?php
+$array ="Nill niti";
+
+$newarray = str_split($array);
+
+echo"<pre>";
+print_r($newarray);
+echo "</pre>";
+
+$newarray2 = str_split($array,2);
+
+echo"<pre>";
+print_r($newarray2);
+echo "</pre>";
+
+
+?>
---
+++ string/uppercaseavd lowercase convater.php
@@ -0,0 +1,14 @@
+<?php
+$str = "Nill Niti";
+
+$lowercase = strtolower($str);
+echo $lowercase."<br>";
+
+$uppercase = strtoupper($str);
+echo $uppercase."<br>";
+
+$lcfrist = lcfirst($str);
+echo $lcfrist."<br>";
+
+
+?>
